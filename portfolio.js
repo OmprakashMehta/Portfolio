19ffac28f4c28ceedcc607b9675d44ca1b0fe351
OmprakashMehta <106869544+OmprakashMehta@users.noreply.github.com>
2024-06-11 13:23:57 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,16 @@ function toggle() {
 function toggleout() {
   document.getElementById("mobilemenu").style.display = "none";
 }
+
+// ---scroll-to-top icon use js code----//
+
+function gototop() {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+}
+
+// ---current date & year set js code----//
+
+let date = new Date();
+let year = date.getFullYear();
+
+document.getElementById("year").innerText = year;
